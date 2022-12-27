f573d64b6f4f5b0eb328ce452cf0525a5f88a705
Aartisoni3515 <aartisoni3517@gmail.com>
2022-12-27 12:42:18 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { css } from "styled-components";
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
+import Feature from "./components/Feature";
+import Service from "./components/Service";
+import Price from "./components/Price";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+`;
+
+const Shape = css`
+width: 100%;
+height: 100%;
+position: absolute;
+top: 0;
+left: 0;
+z-index: -1;
+`;
+
+const IntoShape = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  clip-path: polygon(60% 0, 100% 0%, 100% 100%, 49% 100%);
+  background-color: crimson;
+`;
+
+
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 50% 0%, 40% 100%, 0 100%);
+  background-color: black;
+`;
+
+
+const PriceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
+  background-color: crimson;
+`;
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Navbar />
+        <Intro />
+        <IntoShape />
+      </Container>
+      <Container>
+        <Feature />
+        <FeatureShape/>
+      </Container>
+      <Container>
+        <Service/>
+      </Container>
+      <Container>
+      <Price/>
+        <PriceShape/>
+      </Container>
+      <Container>
+        <Footer/>
+        <Contact/>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import styled from "styled-components";
+
+
+const Container = styled.div`
+height:50px;
+/* background-color: aliceblue; */
+`
+
+
+const Wrapper = styled.div`
+padding: 10px 20px;
+display:flex;
+align-items: center;
+justify-content:space-between; `
+
+const Left = styled.div`
+ width:60% ;
+ display:flex;
+ align-items: center;
+justify-content: space-between;`
+ 
+
+
+const Logo = styled.h1`
+font-weight:bold;
+text-decoration:underline crimson;`
+
+const Menu = styled.ul`
+display: flex;
+justify-content: space-evenly;
+list-style: none;
+`
+
+
+const MenuItem = styled.li`
+margin-right: 30px;
+font-size: 20px;
+font-weight: bold;
+color: gray;
+`
+const Button = styled.button`
+border: 2px solid white;
+margin-right: 20px;
+padding:10px 15px ;
+font-weight: bold;
+background-color: crimson;
+color: white;
+border-radius: 10px;
+cursor: pointer;
+`
+
+
+
+const Navbar = () => {
+  return (
+    <>
+     <Container>
+     <Wrapper> 
+     <Left>
+    <Logo> Agency </Logo>
+     <Menu> 
+     <MenuItem>Home</MenuItem>
+     <MenuItem>Features</MenuItem>
+     <MenuItem>Services</MenuItem>
+     <MenuItem>Pricing</MenuItem>
+     <MenuItem>Contact</MenuItem>
+
+     </Menu>
+     </Left>
+     <Button>Join Today</Button>
+
+     
+     
+       </Wrapper>
+     </Container> 
+    
+    </>
+  )
+}
+
+export default Navbar
